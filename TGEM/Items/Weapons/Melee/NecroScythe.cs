efafa253808ce3c889280615da21b4bc5e402019
zoroarkcity <zoroarkcity2002@gmail.com>
2016-09-05 10:34:19 -0700

    Necro Melee Items
    Here comes dat boi

---
+++ TGEM/Items/Weapons/Melee/NecroScythe.cs
@@ -0,0 +1,45 @@
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace TGEM.Items.Weapons.Melee
+{
+    public class NecroScythe : ModItem
+    {
+        public override void SetDefaults()
+        {
+            item.name = "Reaper's Scythe";
+            item.damage = 47;
+            item.melee = true;
+            item.width = 58;
+            item.height = 52;
+            item.toolTip = "Tears through souls.";
+            item.useTime = 25;
+            item.useAnimation = 25;
+            item.useStyle = 1;
+            item.knockBack = 6.5f;
+            item.value = 10000;
+            item.rare = 2;
+            item.useSound = 1;
+            item.autoReuse = true;
+        }
+
+        public override void OnHitNPC(Player player, NPC target, int damage, float knockback, bool crit)
+        {
+            if (Main.rand.Next(4) == 0)
+            {
+                target.AddBuff(153, 360, false);
+            }
+        }
+
+        public override void AddRecipes()
+        {
+            ModRecipe recipe = new ModRecipe(mod);
+            recipe.AddIngredient(null, "NecroBar", 12);
+            recipe.AddTile(TileID.DemonAltar);
+            recipe.SetResult(this);
+            recipe.AddRecipe();
+        }
+    }
+}
