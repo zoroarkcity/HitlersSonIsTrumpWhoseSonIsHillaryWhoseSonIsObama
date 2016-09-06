2a40fe8c8a1c918347d8afe8e97e81b2f899c275
ColinAV516 <colinav516@gmail.com>
2016-09-05 20:08:02 -0400

    Hellstone Glaive creates a mini fire explosion

--- Projectiles/Thrown/HellstoneGlaive.cs
+++ Projectiles/Thrown/HellstoneGlaive.cs
@@ -16,7 +16,7 @@ namespace TGEM.Projectiles.Thrown
             projectile.name = "Hellstone Glaive";
             projectile.width = 22;       //projectile width
             projectile.height = 22;
-            projectile.penetrate = 1;
+            projectile.penetrate = 2;
             aiType = ProjectileID.Shuriken;
             projectile.ignoreWater = true;
             projectile.timeLeft = 6000;
@@ -25,6 +25,7 @@ namespace TGEM.Projectiles.Thrown
         public override void OnHitNPC(NPC target, int damage, float knockback, bool crit)
         {
                 target.AddBuff(24, 180, false);
+			Projectile.NewProjectile(projectile.position.X, projectile.position.Y, 0f, 0f, mod.ProjectileType("HellstoneGlaiveBoom"), projectile.damage, 0f, projectile.owner, 0f, 0f);
         }
 
         public override void OnHitPvp(Player target, int damage, bool crit)
@@ -35,8 +36,8 @@ namespace TGEM.Projectiles.Thrown
             }
         }
     }
-    // This .cs file has 2 classes in it, which is totally fine. (What is important is that namespace+classname is unique. Remember that autoloaded textures follow the namespace+classname convention as well.)
-    // This is an approach you can take to fit your organization style.
+
+	
     public class HellstoneGlaive : ModItem
     {
 
@@ -49,6 +50,8 @@ namespace TGEM.Projectiles.Thrown
             item.rare = 4;
             item.shootSpeed = 15f;
 			item.autoReuse = true;
+			item.useTime = 25;
+			item.useAnimation = 25;
         }
 
         public override void AddRecipes()
@@ -60,4 +63,43 @@ namespace TGEM.Projectiles.Thrown
             recipe.AddRecipe();
         }
     }
+	
+		public class HellstoneGlaiveBoom : ModProjectile
+	{
+		public override void SetDefaults()
+		{
+			projectile.name = "Hellstone Glaive";
+			projectile.width = 55;
+			projectile.height = 55;
+			projectile.aiStyle = 2;
+			projectile.penetrate = -1;
+			projectile.thrown = true;
+			projectile.timeLeft = 3;
+			projectile.friendly = true;
+			projectile.alpha = 255;
+			projectile.tileCollide = false;
+		}
+		public override bool PreAI()
+	{
+		int amountOfProjectiles = 5;
+		for (int i = 0; i < amountOfProjectiles; ++i)
+		{
+			projectile.tileCollide = false;
+			int dust;
+			dust = Dust.NewDust(projectile.position + projectile.velocity, projectile.width, projectile.height, 6, projectile.velocity.X * 0.5f, projectile.velocity.Y * 0.5f);
+			Main.dust[dust].scale = 2.5f;
+			Main.dust[dust].noGravity = true;
+		}
+		return false;
+	}
+		public virtual bool OnTileCollide(Vector2 oldVelocity)
+		{
+			return false;
+		}
+
+		public override void OnHitNPC(NPC target, int damage, float knockback, bool crit)
+		{
+				target.AddBuff(BuffID.OnFire, 180, false);
+		}
+}
 }
