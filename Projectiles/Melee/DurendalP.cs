e503f841fec4402d3ddb4fb346c42bf82a62fd41
GameBro1 <xxlk106xx@gmail.com>
2016-10-05 19:03:40 -0400

    Oh yeah, Mr.Krabs

---
+++ Projectiles/Melee/DurendalP.cs
@@ -0,0 +1,44 @@
+using Microsoft.Xna.Framework;
+using System;
+using Microsoft.Xna.Framework.Graphics;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace TGEM.Projectiles.Melee
+{
+    public class DurendalP : ModProjectile
+    {
+        public override void SetDefaults()
+        {
+            projectile.name = "Durendal Beam";  //projectile name
+            projectile.width = 23;       //projectile width
+            projectile.height = 23;  //projectile height
+            projectile.friendly = true;      //make that the projectile will not damage you
+            projectile.melee = true;         // 
+            projectile.tileCollide = true;   //make that the projectile will be destroed if it hits the terrain
+            projectile.penetrate = -1;      //how many npc will penetrate
+            projectile.timeLeft = 200;   //how many time this projectile has before disepire
+            projectile.light = 1.25f;    // projectile light
+            projectile.extraUpdates = 1;
+            projectile.ignoreWater = true;
+        }
+
+        public override void OnHitNPC(NPC target, int damage, float knockback, bool crit)
+        {
+            if (Main.rand.Next(1) == 0)
+            {
+                target.AddBuff(44, 360, false);
+            }
+
+            if (Main.rand.Next(5) == 0)
+            {
+                target.AddBuff(46, 360, false);
+            }
+        }
+        public override void AI()           //this make that the projectile will face the corect way
+        {                                                           // |
+            projectile.rotation = (float)Math.Atan2((double)projectile.velocity.Y, (double)projectile.velocity.X) + 0.785f;
+        }
+    }
+}
