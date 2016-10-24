98e580c1365d0495ab33f65ebf2d3c757a98d5c4
zoroarkcity <zoroarkcity2002@gmail.com>
2016-10-23 18:34:31 -0700

    Finished what I started probably

--- Items/Weapons/Ranged/GraniteRifle.cs
+++ Items/Weapons/Ranged/GraniteRifle.cs
@@ -31,5 +31,13 @@ namespace TGEM.Items.Weapons.Ranged
             item.shootSpeed = 12f;
             item.useAmmo = ProjectileID.Bullet;
         }
+        public override bool Shoot(Player player, ref Vector2 position, ref float speedX, ref float speedY, ref int type, ref int damage, ref float knockBack)
+        {
+            // Convert Musket Balls into Granite Shots
+            if (type == ProjectileID.Bullet)
+            {
+                type = mod.ProjectileType("GraniteBullet");
+            }
+        }
     }
 }
